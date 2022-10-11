d151efa414336dbbd4828e37fe21af03b063a2fd
straxico <straxico@gmail.com>
2022-10-11 16:45:19 +0330

    init

---
+++ setup.sh
@@ -0,0 +1,7 @@
+apt  install docker.io -y whatever
+apt  install docker-compose -y whatever
+apt install npm -y whatever
+cat /proc/sys/kernel/random/uuid
+uuid=$(cat /proc/sys/kernel/random/uuid)
+echo "UUID:" $uuid
+python ./vmess.py
